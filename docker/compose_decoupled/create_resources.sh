e1b3ff78fbe6a9d7da2b02286800c5ef2aadf05e
mikyll <righi.michy@gmail.com>
2025-06-17 16:00:26 +0200

    fix(docker): compose_decoupled example

--- docker/compose_decoupled/create_resources.sh
+++ docker/compose_decoupled/create_resources.sh
@@ -7,7 +7,7 @@ ADMIN_APIKEY="1a8fe9bd-73ab-493c-ac82-44db40eab641"
 curl -s -i -X PUT "http://localhost:9180/apisix/admin/upstreams/internal_httpbin" -H "X-API-KEY: $ADMIN_APIKEY" -d '
 {
   "nodes": {
-    "httpbin-decoupled:80": 1
+    "httpbin:80": 1
   },
   "type": "roundrobin"
 }'
