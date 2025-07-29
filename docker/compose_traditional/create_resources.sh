e7127710fa1faf64c7b8891be2b6ebbb329276ed
mikyll <righi.michy@gmail.com>
2025-07-29 10:50:49 +0200

    fix(docker): update compose_traditional
    compose.yaml:
    - add name
    - remove container_name
    - remove Etcd useless env vars
    
    scripts to manage resources via REST API

--- docker/compose_traditional/create_resources.sh
+++ docker/compose_traditional/create_resources.sh
@@ -4,27 +4,27 @@ ADMIN_APIKEY="1a8fe9bd-73ab-493c-ac82-44db40eab641"
 
 # Upstreams
 # Upstream to internal service
-curl -s -i -X PUT "http://localhost:9180/apisix/admin/upstreams/internal_httpbin" -H "X-API-KEY: $ADMIN_APIKEY" -d '
+curl -s -i -X PUT "http://localhost:9180/apisix/admin/upstreams/httpbin_internal" -H "X-API-KEY: $ADMIN_APIKEY" -d '
 {
   "nodes": {
-    "httpbin-traditional:80": 1
+    "httpbin:80": 1
   },
   "type": "roundrobin"
 }'
 
 # Routes
 # Simple route to internal service
-curl -s -i -X PUT "http://localhost:9180/apisix/admin/routes/base_internal" -H "X-API-KEY: $ADMIN_APIKEY" -d '
+curl -s -i -X PUT "http://localhost:9180/apisix/admin/routes/internal_base" -H "X-API-KEY: $ADMIN_APIKEY" -d '
 {
   "uri": "/anything",
-  "upstream_id": "internal_httpbin"
+  "upstream_id": "httpbin_internal"
 }'
 
 # Route with regex URI transformation
-curl -s -i -X PUT "http://localhost:9180/apisix/admin/routes/httpbin" -H "X-API-KEY: $ADMIN_APIKEY" -d '
+curl -s -i -X PUT "http://localhost:9180/apisix/admin/routes/internal_httpbin" -H "X-API-KEY: $ADMIN_APIKEY" -d '
 {
   "uri": "/httpbin/*",
-  "upstream_id": "internal_httpbin",
+  "upstream_id": "httpbin_internal",
   "plugins": {
     "proxy-rewrite": {
       "regex_uri": [
